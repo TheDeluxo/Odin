10c72e41e8a5c849fe5c620b1bb5fb89e12f6a62
Dimitar <thedeluxo@gmail.com>
2024-01-31 21:18:47 +0200

    starting js ...again

---
+++ js_test/index.js
@@ -0,0 +1,13 @@
+console.log(68 + 1)
+let seq = [1, 22, 23, 24, 25, 26]
+console.log(seq)
+console.log((4 + 6 +9) / 77)
+let a = 10;
+console.log(a)
+console.log(a * 9)
+let b = 7 * a;
+console.log(b)
+const MAX = 57
+var actual = MAX - 13
+var percentage = actual/MAX
+console.log(percentage)
