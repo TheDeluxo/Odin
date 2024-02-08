001811a6802ed96d3221d46c347d8da6f94dd192
deluxo <thedeluxo@gmail.com>
2024-02-09 01:14:10 +0200

    fuuuck

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    if(num < 0){
+        return "ERROR"
+    }
+    answ = "";
+    for(i = 0; i < num; i++){
+        answ += str
+    }
+    return answ
 };
 
 // Do not edit below this line
--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    rts = ""
+    for(i = 0; i < str.length; i++){
+        // rts += ttt[ttt.length -1]
+        rts += substr(str[str.length-1],0+i)
+        // t = str.replace(str[str.length], "")
+    }
+    return rts
 };
 
 // Do not edit below this line
