e7b75546aa3f3ba7cb3720c79308f0ab09bf57b3
Dimitar <thedeluxo@gmail.com>
2024-03-06 17:30:14 +0200

    advanced js but .reduce( ) is pain in the ass

--- js_test/object.js
+++ js_test/object.js
@@ -92,3 +92,35 @@ function multiplyNumeric(menu){
 multiplyNumeric(menu)
 console.log(menu.width);
 console.log(menu.height);
+
+
+
+
+// exercise before assignment https://www.theodinproject.com/lessons/foundations-object-basics
+const arr = [1, 2, 3, 4, 5];
+
+function sumOfTripledEvens(array) {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      // Step 1: If the element is an even number
+      if (array[i] % 2 === 0) {
+        // Step 2: Multiply this number by three
+        const tripleEvenNumber = array[i] * 3;
+  
+        // Step 3: Add the new number to the total
+        sum += tripleEvenNumber;
+      }
+    }
+    return sum;
+  }
+
+console.log("Original func: " + sumOfTripledEvens(arr))
+
+function sumOfTripledEvens(array) {
+    return array
+      .filter((num) => num % 2 === 0)
+      .map((num) => num * 3)
+      .reduce((acc, curr) => acc + curr);
+}
+
+console.log("New func: " + sumOfTripledEvens(arr))
